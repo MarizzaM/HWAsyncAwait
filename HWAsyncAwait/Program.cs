3b0ece2dece6272d72e00aa1a56d068858484bb6
User <User@DESKTOP-0BJFLEG>
2021-03-06 22:07:55 +0200

    Add project files.

---
+++ HWAsyncAwait/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HWAsyncAwait
+{
+    class Program
+    {
+        private static async Task WriteToTextToFile()
+        {
+            // Write the specified text asynchronously to a new file named "WriteTextAsync.txt".
+            using (StreamWriter outputFile = new StreamWriter(@"c:\temp\WriteTextAsync.txt"))
+            {
+                for (int i = 1; i <= 1000; i++)
+                {
+                    await outputFile.WriteAsync($"{i.ToString()} ");
+                    Thread.Sleep(2);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Task task = WriteToTextToFile();
+
+            while (!task.Wait(100))
+            {
+                Console.Write(".");
+            }
+
+        }
+    }
+}
